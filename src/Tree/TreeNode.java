9900ef07a94e29a171bbd5c399172238ae087b87
Zhuoyun Jin <zjin@transperfect.com>
2018-02-05 13:24:03 -0500

    02/05/2018

--- src/Tree/TreeNode.java
+++ src/Tree/TreeNode.java
@@ -6,10 +6,20 @@ public class TreeNode {
 	public TreeNode right;
 	public TreeNode(int x) { val = x; }
 	
-	public TreeNode(int[] arr){
+	public static TreeNode buildTree(int[] arr){
 		int len = arr.length;
-		if(len==0) return;
-		
+		if(len ==0) return null;
+		return buildTreeHelper(arr, 0, len-1);
+	}
+	
+	public static TreeNode buildTreeHelper(int[] arr, int start, int end){
+		if(start<end) return null;
+
+		int mid = (start+end)/2;
+		TreeNode treeNode = new TreeNode(arr[mid]);
+		treeNode.left=buildTreeHelper(arr, 0, mid-1);
+		treeNode.right = buildTreeHelper(arr, mid+1, arr.length-1);
+		return treeNode;
 	}
 }
 
