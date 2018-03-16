ecf815f7551779d65aeba42bbe6d6ffd2577d2d1
Zhuoyun Jin <zjin@transperfect.com>
2018-03-16 14:53:33 -0400

    03/16/2018

--- src/LinkedList/ReverseNodeInKGroup.java
+++ src/LinkedList/ReverseNodeInKGroup.java
@@ -23,7 +23,7 @@ public class ReverseNodeInKGroup {
 		ListNode tailCopy=current;
 		ListNode next = current.next;
 		for(int i=0;i<k;i++){
-			System.out.println(i);
+			//System.out.println(i);
 			current.next=tail;
 			tail=current;
 			current=next;
@@ -35,6 +35,40 @@ public class ReverseNodeInKGroup {
 	}
 
 	public ListNode reverseKGroup2(ListNode head, int k) {
+		ListNode start= new ListNode(0);
+		start.next = head;
+		reverse(start, k);
+		return start.next;
+	}
 
+	public void reverse(ListNode start, int k) {
+		if(start.next==null|| start.next.next ==null) return;
+		ListNode tail = start;
+		for(int i=0;i<k;i++) {
+			if(tail.next == null) return;
+			tail = tail.next;
+		}
+		ListNode endNode = tail.next;
+		ListNode head = start.next;
+		ListNode headCopy = head;
+		start.next = tail;
+		ListNode next = head.next;
+		for(int i=0;i<k-1;i++) {
+			ListNode nextNext = next.next;
+			next.next = head;
+			head = next;
+			next = nextNext;
+			//System.out.println(head.val);
+		}
+		
+		headCopy.next = endNode;
+		reverse(headCopy, k);
+	}
+
+	public static void main(String[] args) {
+		ReverseNodeInKGroup test = new ReverseNodeInKGroup();
+		ListNode head = ListNode.buildList(new int[] {1,2});
+		ListNode head2 =test.reverseKGroup2(head, 5);
+		ListNode.print(head2);
 	}
 }
