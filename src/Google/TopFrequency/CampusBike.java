72e23a8ef952a1b2b98ebac6c572af234f7cad52
Zhuoyun Jin <zj285@nyu.edu>
2019-09-13 01:34:37 +0200

    Added CampusBike and CountCompleteTreeNode

--- src/Google/TopFrequency/CampusBike.java
+++ src/Google/TopFrequency/CampusBike.java
@@ -1,7 +1,62 @@
 package Google.TopFrequency;
 
-public class CampusBike {
-    public int[] assignBikes(int[][] workers, int[][] bikes) {
+import java.util.Comparator;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.*;
 
+class CampusBikeComparator implements Comparator<int[]> {
+    public int compare(int[] pair1, int[] pair2)
+    {
+        if(pair1[0]< pair2[0]) return -1;
+        else if(pair1[1] < pair2[1]) return -1;
+        return 1;
     }
+}
+
+public class CampusBike {
+
+    public int[] assignBikes(int[][] workers, int[][] bikes) {
+        int[] result = new int[workers.length];
+
+        Set<Integer> availableWorkers = new HashSet<>();
+        for(int i=0;i<workers.length;i++){
+            availableWorkers.add(i);
+        }
+
+        Set<Integer> availableBikes = new HashSet<>();
+        for(int i=0;i<bikes.length;i++){
+            availableBikes.add(i);
+        }
+
+        Map<Integer, TreeSet<int[]>> distanceMap = new TreeMap<>();
+        int[][] distance = new int[workers.length][bikes.length];
+        for(int i=0;i<workers.length;i++){
+            for(int j=0;j<bikes.length;j++){
+                distance[i][j] = getManhattanDistance(workers[i][0],workers[i][1], bikes[j][0], bikes[j][1]);
+                if(!distanceMap.containsKey(distance[i][j])){
+                    distanceMap.put(distance[i][j], new TreeSet<>(new CampusBikeComparator()));
+                }
+                distanceMap.get(distance[i][j]).add(new int[]{i, j});
+            }
+        }
+
+        for(Map.Entry<Integer, TreeSet<int[]>> entry : distanceMap.entrySet()){
+            for(int[] pair : entry.getValue()){
+                int workerNumber = pair[0];
+                int bikeNumber = pair[1];
+                if(availableBikes.contains(bikeNumber) && availableWorkers.contains(workerNumber)){
+                    result[workerNumber] = bikeNumber;
+                    availableBikes.remove(bikeNumber);
+                    availableWorkers.remove(workerNumber);
+                }
+            }
+        }
+        return result;
+    }
+
+    public int getManhattanDistance(int p1X, int p1Y, int p2X, int p2Y){
+        return Math.abs(p1X-p2X) + Math.abs(p1Y-p2Y);
+    }
+
 }
--- src/Google/TopFrequency/CountCompleteTreeNodes.java
+++ src/Google/TopFrequency/CountCompleteTreeNodes.java
@@ -11,57 +11,26 @@ class TreeNode {
 
 public class CountCompleteTreeNodes {
 
-    // Return tree depth in O(d) time.
-    public int computeDepth(TreeNode node) {
-        int d = 0;
-        while (node.left != null) {
-            node = node.left;
-            ++d;
+    public int countNodes(TreeNode root){
+        if(root == null) return 0;
+        int leftHeight = getLeftHeight(root.left);
+        int rightHeight = getRightHeight(root.right);
+        if(leftHeight == rightHeight) {
+            return (int)Math.pow(2, rightHeight+1)-1;
         }
-        return d;
+        else{
+            return 1+ countNodes(root.left) + countNodes(root.right);
+        }
     }
 
-    // Last level nodes are enumerated from 0 to 2**d - 1 (left -> right).
-    // Return True if last level node idx exists.
-    // Binary search with O(d) complexity.
-    public boolean exists(int idx, int d, TreeNode node) {
-        int left = 0, right = (int)Math.pow(2, d) - 1;
-        int pivot;
-        for(int i = 0; i < d; ++i) {
-            pivot = left + (right - left) / 2;
-            if (idx <= pivot) {
-                node = node.left;
-                right = pivot;
-            }
-            else {
-                node = node.right;
-                left = pivot + 1;
-            }
-        }
-        return node != null;
+    public int getRightHeight(TreeNode root){
+        if(root == null) return 0;
+        return getRightHeight(root.right) + 1;
     }
 
-    public int countNodes(TreeNode root) {
-        // if the tree is empty
-        if (root == null) return 0;
-
-        int d = computeDepth(root);
-        // if the tree contains 1 node
-        if (d == 0) return 1;
-
-        // Last level nodes are enumerated from 0 to 2**d - 1 (left -> right).
-        // Perform binary search to check how many nodes exist.
-        int left = 1, right = (int)Math.pow(2, d) - 1;
-        int pivot;
-        while (left <= right) {
-            pivot = left + (right - left) / 2;
-            if (exists(pivot, d, root)) left = pivot + 1;
-            else right = pivot - 1;
-        }
-
-        // The tree contains 2**d - 1 nodes on the first (d - 1) levels
-        // and left nodes on the last level.
-        return (int)Math.pow(2, d) - 1 + left;
+    public int getLeftHeight(TreeNode root){
+        if(root == null) return 0;
+        return getLeftHeight(root.left) + 1;
     }
 
     public int countNodesNaive(TreeNode root) {
