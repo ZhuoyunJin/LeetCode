aaab5418e349a92ba35942568ca873fbe0842898
Zhuoyun 🏂  Jin <zhuoyuj@amazon.com>
2019-11-13 01:18:05 +0100

    11/13/2019

--- src/Google/TopFrequency/StringTransformsIntoAnotherString.java
+++ src/Google/TopFrequency/StringTransformsIntoAnotherString.java
@@ -1,35 +1,31 @@
 package Google.TopFrequency;
 
+import java.util.*;
+
 public class StringTransformsIntoAnotherString {
     public boolean canConvert(String str1, String str2) {
-
-        char[] map = new char[26];
-        int[] circle = new int[26];
+        if(str1.equals(str2)) return true;
+        Map<Character, Character> map = new HashMap<>();
+        Set<Character> set = new HashSet<>();
         for(int i=0;i< str1.length();i++){
             char char1 = str1.charAt(i);
             char char2 = str2.charAt(i);
-            if(map[char1-'a'] != 0 && map[char1-'a'] == char2){
+            if(map.containsKey(char1) && map.get(char1) != char2){
                 return false;
-            } else {
-                map[char1-'a'] = char2;
+            } else if(!map.containsKey(char1)){
+                map.put(char1, char2);
+                set.add(char2);
             }
         }
+        return set.size()<26;
 
-        int numberOfCircle = 1;
-        for(int i=0;i<26;i++){
-            circle[i] = numberOfCircle;
-            char currentLetter = map[i];
-            while(currentLetter > 0 && circle[currentLetter-'a'] == 0){
-                circle[currentLetter-'a'] = numberOfCircle;
-                currentLetter = map[currentLetter-'a'];
-            }
-            
-        }
 
     }
 
+
     public static void main(String[] args){
         StringTransformsIntoAnotherString test = new StringTransformsIntoAnotherString();
-        test.canConvert("aabcc", "ccdee");
+        boolean canConvert = test.canConvert("abcdefghijklmnopqrstuvwxyz", "bcdefghijklmnopqrstuvwxyzq");
+        System.out.println(canConvert);
     }
 }
